d2ec5e0a6a92a08fcb0ab84abe43da01ceaba08a
Parton <42082250+NotDonaldTrump@users.noreply.github.com>
2020-10-23 23:58:21 +1100

    yesss

--- cache/compiled/files/79d9599002244f500238117f34a32536.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'C:/xampp/htdocs/trumpbot.xyz/user/config/security.yaml',
-    'modified' => 1586328681,
-    'data' => [
-        'salt' => 'gpudNblJ2sK5Mc'
-    ]
-];
